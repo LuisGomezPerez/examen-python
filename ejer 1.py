4ac0326c4b40715c5919da21bca4c67cc9fd27e4
Luis Gómez <luis.gomezperez@usp.cue.es>
2023-01-09 16:20:37 +0100

    h

---
+++ ejer 1.py
@@ -0,0 +1,11 @@
+import pickle
+class  Stormtrooper.py():
+    def __init__(self,nombre,rango):
+        self.nombre=nombre
+        self.rango=rango
+    
+def __str__(self):
+    return self.nombre
+
+def __str__(self):
+    return self.rango
