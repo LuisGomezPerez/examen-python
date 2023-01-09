7230fe25393dcd23aa8e58a3515fb8377b6261a1
Luis Gómez <luis.gomezperez@usp.cue.es>
2023-01-09 17:19:44 +0100

    h

---
+++ ejer 5.py
@@ -0,0 +1,29 @@
+#a
+def funcion1(mochila):
+    for x in mochila:
+        if x!= "sable":
+            print (x)
+    return
+funcion1
+#b
+def funcion2(mochila):
+    cont=0
+    for x in mochila:
+        if x!= "sable":
+            cont=cont+1
+    print(cont)
+    return
+funcion2
+def funcion3(mochila):
+    if "sable" not in mochila
+    print("no esta")
+    return
+funcion3
+#c
+mochila=["pistola","balon","cuaderno"]
+
+
+
+
+
+   
